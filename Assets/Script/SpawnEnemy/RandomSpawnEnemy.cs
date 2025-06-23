92a3b7c76292ff9a4a3d9d9f75f66cc3bd109bb8
Haru2065 <146793398+Haru2065@users.noreply.github.com>
2025-06-23 11:27:56 +0900

    ハクスラシステムの実装途中段階

---
+++ Assets/Script/SpawnEnemy/RandomSpawnEnemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ハクスラシステムランダムで敵を生成する
+/// </summary>
+public class RandomSpawnEnemy : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("敵情報をCSVから取得")]
+    private string enemyDataCSV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
